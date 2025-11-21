bf379f1f00f2fdad9fad7dc18a9fc07d284be815
kwonyg <kyg930203@naver.com>
2025-11-22 00:23:09 +0900

    LockFreeQueue 버그 수정
    Pop 시도시 head 메모리가 해제된 상태일 수 있어 해제시점 맞추기 위해 shared_ptr로 변경

--- Core/LockFreeQueue.h
+++ Core/LockFreeQueue.h
@@ -9,13 +9,15 @@ namespace bugat
         {
             Node() : _next(nullptr) {}
             T _value;
-            std::atomic<Node*> _next;
+            std::atomic<std::shared_ptr<Node>> _next;
         };
 
     public:
         LockFreeQueue() : _size(0)
         {
-            _head = _tail = new Node();
+            auto temp = new Node();
+            _head = std::shared_ptr<Node>(temp);
+            _tail = temp;
         }
 
         void Push(T& value)
@@ -25,8 +27,8 @@ namespace bugat
             node->_next = nullptr;
 
             auto tail = _tail.load(std::memory_order_acquire);
-            while (false == _tail.compare_exchange_strong(tail, node));
-            tail->_next.store(node, std::memory_order_release);
+            while (false == _tail.compare_exchange_strong(tail, node, std::memory_order_acq_rel, std::memory_order_acquire));
+            tail->_next.store(std::shared_ptr<Node>(node), std::memory_order_release);
 
             _size.fetch_add(1, std::memory_order_release);
         }
@@ -38,8 +40,8 @@ namespace bugat
             node->_next = nullptr;
 
             auto tail = _tail.load(std::memory_order_acquire);
-            while (false == _tail.compare_exchange_strong(tail, node));
-            tail->_next.store(node, std::memory_order_release);
+            while (false == _tail.compare_exchange_strong(tail, node, std::memory_order_acq_rel, std::memory_order_acquire));
+            tail->_next.store(std::shared_ptr<Node>(node), std::memory_order_release);
 
             _size.fetch_add(1, std::memory_order_release);
         }
@@ -47,16 +49,16 @@ namespace bugat
         bool Pop(T& output)
         {
             auto head = _head.load(std::memory_order_acquire);
-            Node* popNode = nullptr;
+            std::shared_ptr<Node> popNode = nullptr;
             do
             {
-                if (popNode = head->_next; popNode == nullptr)
+                if (popNode = head->_next.load(std::memory_order_acquire); popNode == nullptr)
                     return false;
-            } while (false == _head.compare_exchange_strong(head, popNode));
+            } while (false == _head.compare_exchange_strong(head, popNode, std::memory_order_acq_rel, std::memory_order_acquire));
             _size.fetch_sub(1, std::memory_order_release);
 
             output = std::move(popNode->_value);
-            delete head;
+            head->_next = nullptr;
 
             return true;
         }
@@ -84,14 +86,14 @@ namespace bugat
         void Clear()
         {
             T node;
-            while (GetSize() <= 0)
+            while (GetSize() > 0)
                 Pop(node);
         }
 
         int64_t GetSize() { return _size; }
 
     private:
-        std::atomic<Node*> _head;
+        std::atomic<std::shared_ptr<Node>> _head;
         std::atomic<Node*> _tail;
         std::atomic<int64_t> _size;
     };
