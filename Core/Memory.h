1121808a3460ce66b78822d56dd3d902d47986f2
kwonyg <kyg930203@naver.com>
2026-01-28 11:09:49 +0900

    RefCountable 멤버 함수 제거, TSharedPtr 버그 수정
    1. RefCountable 멤버 함수로 참조 카운트 증가, 감소, deleter 수정이 있어 상속받은 클래스에서 접근할 수 있어 변경, functor를 만들어 friend 선언 후 접근 하도록 수정
    2. TSharedPtr 이동 대입연산자가 없어 이동 연산 대입연산시 일반 대입 연산자가 호출되어 참조 카운팅이 증가하는 버그 수정

--- Core/Memory.h
+++ Core/Memory.h
@@ -15,33 +15,68 @@ namespace bugat
         T _value;
     };
 
+    class __deleter;
+    struct __add_ref_count;
+    struct __sub_ref_count;
+    struct __set_deleter;
+    struct __get_deleter;
     class RefCountable
     {
-    public:
-        void AddRef()
-        {
-            _refCount.fetch_add(1);
-        }
-        
-        int SubRef()
-        {
-            return _refCount.fetch_sub(1);
-        }
-
-        void SetDeleter(std::function<void(void*)>&& deleter)
-        {
-            _deleter = std::move(deleter);
-		}
-
-        std::function<void(void*)>& GetDeleter() { return _deleter; }
-
+        friend struct __add_ref_count;
+        friend struct __sub_ref_count;
+        friend struct __set_deleter;
+        friend struct __get_deleter;
     protected:
         RefCountable() :_refCount(0), _deleter(nullptr) {}
         virtual ~RefCountable() {}
 
-    protected:
+    private:
         std::atomic<int> _refCount;
+        __deleter* _deleter;
+    };
+
+    class __deleter
+    {
+    public:
+        __deleter(std::function<void(void*)>&& deleter) : _deleter(deleter) {}
+        void operator()(void* ptr)
+        {
+            _deleter(ptr);
+        }
+    private:
         std::function<void(void*)> _deleter;
+    };
+
+    struct __add_ref_count
+    {
+        void operator()(RefCountable* refCountObj)
+        {
+            refCountObj->_refCount.fetch_add(1);
+        }
+    };
+
+    struct __sub_ref_count
+    {
+        int operator()(RefCountable* refCountObj)
+        {
+            return refCountObj->_refCount.fetch_sub(1);
+        }
+    };
+
+    struct __set_deleter
+    {
+        void operator()(RefCountable* refCountObj, __deleter* deleter)
+        {
+            refCountObj->_deleter = deleter;
+        }
+    };
+
+    struct __get_deleter
+    {
+        __deleter* operator()(RefCountable* refCountObj)
+        {
+            return refCountObj->_deleter;
+        }
     };
 
     template<typename T>
@@ -50,22 +85,28 @@ namespace bugat
     public:
         TSharedPtr(T* ptr = nullptr) : _ptr(ptr)
         {
-            if (_ptr) _ptr->AddRef();
+            if (_ptr) __add_ref_count()(_ptr);
         }
 
         TSharedPtr(T* ptr, const std::function<void(T*)>& deleter) : _ptr(ptr)
         {
-            if (_ptr) _ptr->AddRef();
-            if(deleter)
-                _ptr->SetDeleter([deleter](void* ptr) {
-				    T* tptr = static_cast<T*>(ptr);
-				    deleter(tptr);
-                    });
+            if (_ptr)
+            {
+                __add_ref_count()(_ptr);
+                if (deleter)
+                {
+                    auto deleterPtr = new __deleter([deleter](void* ptr) {
+                        T* tptr = static_cast<T*>(ptr);
+                        deleter(tptr);
+                        });
+                    __set_deleter()(_ptr, deleterPtr);
+                }
+            }
         }
 
 		TSharedPtr(const TSharedPtr& other) : _ptr(other._ptr)
         {
-            if (_ptr) _ptr->AddRef();
+            if (_ptr) __add_ref_count()(_ptr);
         }
 
         TSharedPtr(TSharedPtr&& other) : _ptr(other._ptr)
@@ -76,7 +117,7 @@ namespace bugat
         template<typename U>
         TSharedPtr(const TSharedPtr<U>& other) : _ptr(other.Get())
         {
-            if (_ptr) _ptr->AddRef();
+            if (_ptr) __add_ref_count()(_ptr);
         }
 
         ~TSharedPtr()
@@ -100,8 +141,20 @@ namespace bugat
             {
                 Reset();
                 _ptr = other._ptr;
-                if (_ptr) _ptr->AddRef();
+                if (_ptr) __add_ref_count()(_ptr);
             }
+            return *this;
+        }
+
+        TSharedPtr& operator=(TSharedPtr&& other)
+        {
+            if (this == &other)
+                return *this;
+
+            Reset();
+            _ptr = other._ptr;
+            other._ptr = nullptr;
+
             return *this;
         }
 
@@ -144,10 +197,13 @@ namespace bugat
         {
             if (_ptr)
             {
-                if (_ptr->SubRef() == 1)
+                if (__sub_ref_count()(_ptr) == 1)
                 {
-					if (auto deleter = _ptr->GetDeleter(); deleter)
-                        deleter(_ptr);
+                    if (auto deleter = __get_deleter()(_ptr); deleter)
+                    {
+                        (*deleter)(_ptr);
+                        delete deleter;
+                    }
                     else
                         delete _ptr;
                 }
